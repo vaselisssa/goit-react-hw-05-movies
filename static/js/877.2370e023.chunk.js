32afea1926ac77e3a7aa5de7d2aac1a7eca0b698
vaselisssa <63919071+vaselisssa@users.noreply.github.com>
2023-11-13 00:21:34 +0000

    Deploying to gh-pages from @ vaselisssa/goit-react-hw-05-movies@54e9b14d27632197a54bc606d7bcd1dc773bbe92 🚀

--- static/js/877.2370e023.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[877],{1877:function(e,t,r){r.r(t),r.d(t,{default:function(){return u}});r(2791);var n=r(184),u=function(){return(0,n.jsx)("div",{children:"Cast"})}}}]);
-//# sourceMappingURL=877.2370e023.chunk.js.map
